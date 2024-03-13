f66bfccd5c0da88b374f8cb4d2183ba18a012a65
Сергей <kowqo@yandex.ru>
2024-03-13 22:29:05 +0300

    add more test to hello

--- base/hello/hello_test.go
+++ base/hello/hello_test.go
@@ -3,9 +3,36 @@ package main
 import "testing"
 
 func TestHello(t *testing.T) {
-	got := Hello("Chris")
+	t.Run("if have name Chris, default language", func(t *testing.T) {
+		got := Hello("Chris", "")
+		want := "Hello " + "Chris"
+		assertCorrectMessage(t, got, want)
+	})
 
-	want := "Hello"
+	t.Run("with no args, default language", func(t *testing.T) {
+		got := Hello("", "")
+		want := "Hello " + "World!"
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("args with Russian lang", func(t *testing.T) {
+		const name = "Serega"
+		got := Hello(name, "RU")
+		want := "Privet " + name
+		assertCorrectMessage(t, got, want)
+
+	})
+
+	t.Run("args with Russian lang", func(t *testing.T) {
+		got := Hello("", "RU")
+		want := "Privet " + "Mir"
+		assertCorrectMessage(t, got, want)
+
+	})
+}
+
+func assertCorrectMessage(t testing.TB, got, want string) {
+	t.Helper()
 
 	if got != want {
 		t.Errorf("got %q want %q", got, want)
--- base/hello/main.go
+++ base/hello/main.go
@@ -8,6 +8,25 @@ func main() {
 	fmt.Println(a)
 }
 
-func Hello() string {
-	return "Hello"
+const prefixEn = "Hello "
+const prefixRu = "Privet "
+
+func Hello(name, lang string) string {
+	var pref string
+
+	if name == "" {
+		if lang == "RU" {
+			name = "Mir"
+		} else {
+			name = "World!"
+		}
+	}
+
+	if lang == "RU" {
+		pref = prefixRu
+	} else {
+		pref = prefixEn
+	}
+
+	return pref + name
 }
